3c66467ef8a8fc52413da9c9d314385b3a00de9e
Wei Huang <Wei.Huang@noaa.gov>
2026-06-16 10:54:32 -0500

    changes in hercules

--- dev/container/hosts/hercules/intel/prefix/container_python.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-module load singularity
-LD_LIBRARY_PATH=$(dirname "/scratch3/NCEPDEV/nems/role.epic/containers/ubuntu22.04-intel-ufs-env-v1.9.2.img")
-export LD_LIBRARY_PATH
-
-singularity exec \
-    ${CONTAINER_BINDINGS} \
-    "${CONTAINER_SIF}" \
-    "${HOMEglobal}/dev/container/env/python-env.sh" "$@"
--- dev/container/shell-in-container.sh
+++ dev/container/shell-in-container.sh
@@ -9,6 +9,7 @@ sed -e "s|GLOBALWORKFLOWHOMEDIR|${gwHomeDir}|g" \
        ${gwHomeDir}/dev/container/hosts/${MACHINE_ID}/intel/env/CONTAINER.env \
        > ${gwHomeDir}/env/CONTAINER.env
 source ${gwHomeDir}/env/CONTAINER.env
+cp ${gwHomeDir}/env/CONTAINER.env ${gwHomeDir}/dev/container/env/CONTAINER.env
 
 # shellcheck disable=SC2086
 singularity shell -e ${CONTAINER_BINDINGS} "${CONTAINER_SIF}"
